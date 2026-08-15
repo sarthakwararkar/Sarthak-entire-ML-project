65af371467550fd79945c8ef45bbb4991cc8d4c4
sarthakwararkar <sarthakwararkar2@gmail.com>
2026-08-15 19:14:26 +0530

    fix deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -27,14 +27,14 @@ class PredictPipeline:
 class CustomData:
     def __init__(self ,
                  gender:str,
-                 race_etnicity:str,
+                 race_ethnicity:str,
                  parental_level_of_education,
                  lunch:str,
                  test_preparation_course:str,
                  reading_score:int ,
                  writing_score:int):
         self.gender = gender
-        self.race_etnicity= race_etnicity
+        self.race_ethnicity= race_ethnicity
         self.parental_level_of_education=parental_level_of_education
         self.lunch=lunch
         self.test_preparation_course = test_preparation_course
